02bea30509a65f9fce456fc676859d8a5d5d3502
Zhang Darcy <dx@DXs-MacBook-Pro.local>
2025-08-18 11:09:49 +0800

    outline of renderer done

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from lexer import lexer
 from parser import parse
+from renderer import render
 
 
 test = r"F_n = \frac{1}{\sqrt5} \left[ \left( \frac{1+\sqrt5}2 \right) ^n - \left( \frac{1-\sqrt5}2 \right) ^n \right]"
 test = r"\frac{-b\pm\sqrt{b^2-4ac}}{2a}\pm"
-test = r"\sqrt[2ab]x^2"
-test = r"\sum^4+\int\limits_3^4-\lim_{3+3^2}^4"
+# test = r"\sqrt[2ab]x^2"
+# test = r"\sum^4+\int\limits_3^4-\lim_{3+3^2}^4"
 print(test)
 lexered = lexer(test)
 for token in lexered:
@@ -13,3 +14,27 @@ for token in lexered:
 parsed = parse(lexered)
 for i in range(len(parsed)):
     print(i, parsed[i])
+rendered = render(parsed)
+for i in range(len(rendered)):
+    print(i, rendered[i])
+"""
+0 ('opn_root', ('meta', 'start'), [1, 18], [])
+1 ('cmd_frac', ('cmnd', 'frac'), [2, 15], [])
+2 ('opn_brac', ('symb', '{'), [3, 4, 5, 6], [])
+3 ('txt_leaf', ('symb', '-'), [], [])
+4 ('txt_leaf', ('alph', 'b'), [], [])
+5 ('cmd_leaf', ('cmnd', 'pm'), [], [])
+6 ('cmd_sqrt', ('cmnd', 'sqrt'), [7], [])
+7 ('opn_brac', ('symb', '{'), [8, 11, 12, 13, 14], [])
+8 ('txt_leaf', ('alph', 'b'), [], [9])
+9 ('sup_scrpt', ('symb', '^'), [10], [])
+10 ('txt_leaf', ('numb', '2'), [], [])
+11 ('txt_leaf', ('symb', '-'), [], [])
+12 ('txt_leaf', ('numb', '4'), [], [])
+13 ('txt_leaf', ('alph', 'a'), [], [])
+14 ('txt_leaf', ('alph', 'c'), [], [])
+15 ('opn_brac', ('symb', '{'), [16, 17], [])
+16 ('txt_leaf', ('numb', '2'), [], [])
+17 ('txt_leaf', ('alph', 'a'), [], [])
+18 ('cmd_leaf', ('cmnd', 'pm'), [], [])
+"""
--- renderer.py
+++ renderer.py
@@ -1,15 +1,87 @@
-# functional rendering, from list of nodes to rows of str
-def render_vert_pile(nodes: list) -> list:
+
+
+def render_leaf(token: tuple) -> tuple:
     ...
 
 
-def render_concat(nodes: list) -> list:
+def render_concat(node: list) -> tuple:
     ...
 
 
-def render_script(nodes: list) -> list:
+def render_square_root(node: list) -> tuple:
     ...
+
+
+def render_sup_script(node: list) -> tuple:
+    ...
+
+
+def render_sub_script(node: list) -> tuple:
+    ...
+
+
+def render_fraction(node: list) -> tuple:
+    ...
+
+
+def render_apply_scripts(scripts: list) -> tuple:
+    ...
+
+
+def render_parent(node_type: str, children: list) -> tuple:
+    if node_type == "cmd_sqrt":
+        return render_square_root(children)
+    elif node_type == "cmd_frac":
+        return render_fraction(children)
+    elif node_type == "sup_scrpt":
+        return render_sup_script(children)
+    elif node_type == "sub_scrpt":
+        return render_sup_script(children)
+    else:
+        raise ValueError(f"Undefined node {node_type}")
+
+
+parent_node_types = {
+    "opn_root",
+    "opn_brac",
+    "opn_dlim",
+    "cls_dlim",
+    "sup_scrpt",
+    "sub_scrpt",
+    "cmd_sqrt",
+    "cmd_frac",
+}
+
+leaf_node_types = {
+    "txt_leaf",
+    "cmd_leaf",
+    "ctr_base",
+}
 
 
 def render(nodes: list) -> list:
-    ...
+    canvas = []
+    for i in range(len(nodes)-1, -1, -1):
+        node = nodes[i]
+        node_type = node[0]
+        node_token = node[1]
+        children_ids = node[2]
+        children = []
+        for i in children_ids:
+            children.append(canvas[i])
+        scripts_ids = node[3]
+        scripts = []
+        for i in scripts_ids:
+            scripts.append(canvas[i])
+
+        if node_type in leaf_node_types:
+            sketch, horizon = render_leaf(node_token)
+        elif node_type in parent_node_types:
+            sketch, horizon = render_parent(node_type, children)
+        else:
+            raise ValueError(f"Undefined control sequence {node_token[1]}")
+
+        if scripts:
+            sketch, horizon = render_apply_scripts(scripts)
+        canvas.append(sketch)
+    return canvas[0][0]
