78055a0edfa2385d8af8255963c7b6c5c150a802
dx <darcyzhangdx@gmail.com>
2026-05-03 00:57:45 +0800

    chore(release): bump version to 1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='TeXicode',
-    version='1.0.2',
+    version='1.0.3',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     entry_points={
