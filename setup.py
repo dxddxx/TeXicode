ce6759fef2bdb1fbb4c3c0d466722131ae66021f
dx <darcyzhangdx@gmail.com>
2026-08-11 23:16:43 +0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='TeXicode',
-    version='1.0.3',
+    version='1.0.4',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     entry_points={
